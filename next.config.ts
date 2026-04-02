c0af5acbb58130e200a4a2f7d25794dae865af1c
Samuel D. Monteiro <samuel.dvmonteiro@gmail.com>
2026-04-02 14:44:25 -0300

    build(vercel): Adicionar pasta do chromium explicitamente no next config para ser feito o bundle

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+  outputFileTracingIncludes: {
+    '/api/certificates/[id]/download': [
+      './node_modules/@sparticuz/chromium/**/*',
+    ],
+    '/api/certificates/batch-download': [
+      './node_modules/@sparticuz/chromium/**/*',
+    ],
+  },
 };
 
 export default nextConfig;
